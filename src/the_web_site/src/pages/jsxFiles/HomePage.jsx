315e56e98be02a146384f29a6237b0f8ff0b9e27
Linus Lorensson <ll224hf@student.lnu.se>
2023-12-10 17:24:29 +0100

    Working on the website

--- src/the_web_site/src/pages/jsxFiles/HomePage.jsx
+++ src/the_web_site/src/pages/jsxFiles/HomePage.jsx
@@ -9,49 +9,89 @@ function HomePage() {
     setSelectedOption(option);
   };
 
-  const [starthour, setStarthour] = useState("");
+  const handleButtonClick = (action) => {
+    console.log(`Button ${action} clicked`);
+    // Add your logic here for button actions
+  };
 
+  const [starthour, setStarthour] = useState("");
   const [endhour, setEndhour] = useState("");
 
   const handlestartHourChange = (event) => {
     setStarthour(event.target.value);
   };
 
-  const contentMap = {
-    option1: "Content for Option 1",
-    option2: "Content for Option 2",
-    option3: "Content for Option 3",
-    option4: "Content for Option 4",
-  };
-
   const handleendHourChange = (event) => {
     setEndhour(event.target.value);
   };
 
-  const hours = Array.from({ length: 24 }, (_, index) => index); // generate array from 0 to 23
+  const generateHours = () => {
+    return Array.from({ length: 24 }, (_, index) => index);
+  };
+
+  const contentMap = {
+    option1: (
+      <div>
+        <button onClick={() => handleButtonClick("On")}>On</button>
+        <button onClick={() => handleButtonClick("Off")}>Off</button>
+        <p>Toggle the device on or off.</p>
+      </div>
+    ),
+    option2: (
+      <div>
+        <label>Start Time:</label>
+        <select
+          className="start"
+          value={starthour}
+          onChange={handlestartHourChange}
+        >
+          {generateHours().map((h) => (
+            <option key={h} value={h}>
+              {h < 10 ? `0${h}:00` : `${h}:00`}
+            </option>
+          ))}
+        </select>
+        <br />
+        <label>End Time:</label>
+        <select className="end" value={endhour} onChange={handleendHourChange}>
+          {generateHours().map((h) => (
+            <option key={h} value={h}>
+              {h < 10 ? `0${h}:00` : `${h}:00`}
+            </option>
+          ))}
+        </select>
+        <p>Set a timer for the device.</p>
+      </div>
+    ),
+    option3: (
+      <div>
+        <img
+          src="https://via.placeholder.com/100"
+          alt="Blank 100px by 100px image"
+          style={{ width: "100px", height: "100px" }}
+        />
+        <button onClick={() => handleButtonClick("Option3")}>Click me</button>
+        <p>Display an image and a button.</p>
+      </div>
+    ),
+    option4: (
+      <div>
+        <button onClick={() => handleButtonClick("Shut it down")}>
+          Shut it down
+        </button>
+        <button onClick={() => handleButtonClick("Boot it up")}>
+          Boot it up
+        </button>
+        <p>Control the device power.</p>
+      </div>
+    ),
+  };
 
   const content = contentMap[selectedOption] || "Select an option";
 
   return (
-    /*
-        <div>
-        Timer
-        <select className="start" value={starthour} onChange={handlestartHourChange}>
-        {hours.map((h) => (
-            <option key={h} value={h}>
-            {h < 10 ? `0${h}:00` : `${h}:00`} 
-            </option>
-        ))}
-        </select>
-        <select className="end" value={endhour} onChange={handleendHourChange}>
-        {hours.map((h) => (
-            <option key={h} value={h}>
-            {h < 10 ? `0${h}:00` : `${h}:00`}
-        ))}
-        </select>
-        </div>
-        */
     <div className="container">
+      {/* Options Box */}
       <div className="options-container">
         {options.map((option) => (
           <label key={option} className="option-label">
@@ -66,9 +106,8 @@ function HomePage() {
         ))}
       </div>
 
-      <div className="right-box">
-        <p>{content}</p>
-      </div>
+      {/* Right Box */}
+      <div className="right-box">{content}</div>
     </div>
   );
 }
