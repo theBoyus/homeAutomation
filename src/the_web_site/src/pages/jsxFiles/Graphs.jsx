effd1829d39c713d91f710c45db9dec3c71918c8
Samuel Azzawi <sa224pt@student.lnu.se>
2023-12-10 17:41:35 +0100

    graph work

--- src/the_web_site/src/pages/jsxFiles/Graphs.jsx
+++ src/the_web_site/src/pages/jsxFiles/Graphs.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
+import "../css/Graph.css"
 function Graphs() {
-    
-    return (<div>
-        Graphs page
-    </div>)
+    function importAllImages(r) {
+    return r.keys().map(r);}
+    const images = importAllImages(require.context('../../img', false, /\.(png|jpe?g|svg)$/));
+    const [currentImage, setCurrentImage] = useState(images[0]);
+    console.log(images);
+
+    return (
+        <div>
+            <>selfies with the boys</>
+            <div>
+                {images.map((image, index) => (
+                    <button key={index} onClick={() => setCurrentImage(image)}>
+                        Image {index + 1}
+                    </button>
+                ))}
+            </div>
+            <div className="graphImage">
+                <img src={currentImage} alt="Graph" />
+            </div>
+        </div>
+    );
 }
 
-export default Graphs
+export default Graphs;
