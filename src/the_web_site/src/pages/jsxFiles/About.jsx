cb3668d771a851f07020b74d82dd121c5295dae7
George Warda <gw222fk@sudnet.lnu.se>
2023-12-14 18:48:28 +0100

    txt file with ecxamples on text for about page

--- src/the_web_site/src/pages/jsxFiles/About.jsx
+++ src/the_web_site/src/pages/jsxFiles/About.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import "../css/About.css";
 
 function About() {
@@ -10,8 +9,18 @@ function About() {
           <h3>Who are we</h3>
         </div>
         <div className="txtContainer1">
-          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eleifend sed nisl non dapibus. Praesent finibus eros at libero commodo, eu malesuada erat malesuada. Nulla pulvinar ligula eget neque posuere, in tempor dui pharetra. Nam ut tincidunt felis. Aliquam et ipsum mollis, vulputate libero non, pellentesque nisl. Etiam iaculis scelerisque lectus, et feugiat enim dictum vel. Vestibulum posuere consequat viverra. Phasellus ut sem tempor, feugiat nunc imperdiet, bibendum leo. Curabitur fringilla magna non mi convallis euismod. In hac habitasse platea dictumst. Integer quis tempor purus, a vulputate lacus.
-</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            eleifend sed nisl non dapibus. Praesent finibus eros at libero
+            commodo, eu malesuada erat malesuada. Nulla pulvinar ligula eget
+            neque posuere, in tempor dui pharetra. Nam ut tincidunt felis.
+            Aliquam et ipsum mollis, vulputate libero non, pellentesque nisl.
+            Etiam iaculis scelerisque lectus, et feugiat enim dictum vel.
+            Vestibulum posuere consequat viverra. Phasellus ut sem tempor,
+            feugiat nunc imperdiet, bibendum leo. Curabitur fringilla magna non
+            mi convallis euismod. In hac habitasse platea dictumst. Integer quis
+            tempor purus, a vulputate lacus.
+          </p>
         </div>
       </div>
       <div>
@@ -20,8 +29,11 @@ function About() {
             <h3>Links</h3>
           </div>
           <div className="txtContainer1">
-          <p>Link to our: 
-            <a href="https://gitlab.lnu.se/ll224hf/homeautomation">Repository</a>
+            <p>
+              Link to our:
+              <a href="https://gitlab.lnu.se/ll224hf/homeautomation">
+                Repository
+              </a>
             </p>
           </div>
         </div>
